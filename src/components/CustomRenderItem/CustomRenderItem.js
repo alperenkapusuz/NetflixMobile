58b8fe633e047285369f35f6dc84877f3eac7bd8
Alperen Kapusuz <sefalperen21@gmail.com>
2022-11-26 20:25:21 +0300

    List Screen ve RenderItem stilleri yapıldı

--- src/components/CustomRenderItem/CustomRenderItem.js
+++ src/components/CustomRenderItem/CustomRenderItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, Text, Dimensions} from 'react-native';
+import styles from './CustomRenderItem.style';
 
 const CustomRenderItem = ({item}) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.cover}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: item.images['Poster Art'].url,
+          }}
+        />
+      </View>
+
       <Text>{item.title}</Text>
+      <Text>{item.releaseYear}</Text>
     </View>
   );
 };
--- src/components/CustomRenderItem/CustomRenderItem.style.js
+++ src/components/CustomRenderItem/CustomRenderItem.style.js
@@ -1,5 +1,40 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
 export default StyleSheet.create({
-  container: {},
+  container: {
+    width: Dimensions.get('window').width / 2.1,
+    height: Dimensions.get('window').height / 3.1,
+    backgroundColor: '#E50914',
+    margin: 5,
+    borderWidth: 2,
+    borderColor: '#E50914',
+    borderRadius: 10,
+  },
+  cover: {
+    resizeMode: 'stretch',
+    width: Dimensions.get('window').width / 2.14,
+    height: Dimensions.get('window').height / 4,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: 'black',
+  },
+  title: {
+    height: 50,
+    fontSize: 15,
+  },
+  content: {
+    height: 40,
+  },
+  year: {
+    color: '#D1D1D1',
+    fontSize: 13,
+  },
+  image: {
+    width: Dimensions.get('window').width / 2.15,
+    height: Dimensions.get('window').height / 4,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
 });
